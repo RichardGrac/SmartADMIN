c3603b3e2c1f3ea9f3066017b92aabbb187659c0
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-05-31 23:09:49 -0500

    * Splash screen animation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,25 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import {AngularFireAuth} from "angularfire2/auth";
 
+import {timer} from 'rxjs/observable/timer';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  showSplash = true;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, fireAuth: AngularFireAuth) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      statusBar.backgroundColorByHexString("#0984e3")
+      statusBar.backgroundColorByHexString("#0984e3");
       splashScreen.hide();
+      timer(3000).subscribe(()=>
+        this.showSplash = false
+      );
     });
     fireAuth.auth.signInAnonymously()
       .then(() => { console.log('login success') })
